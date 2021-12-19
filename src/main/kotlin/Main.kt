feb61bb3fd34ef2bf8dc16b37a0e9f6fb6e21f3d
AChebykin <riddle1994@ya.ru>
2021-12-19 11:03:34 +0300

    changed method getWord

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,12 +3,14 @@ val likesWord2 = "лайков"
 val likesWord3 = "лайка"
 
 fun main(){
-    val likes1 = 3
-    val likes2 = 4
-    val likes3 = 53
-    val likes4 = 0
-    val likes5 = 1
-    val likes6 = 10
+    val likes1 = 1
+    val likes2 = 0
+    val likes3 = 21
+    val likes4 = 213
+    val likes5 = 212
+    val likes6 = 111
+    val likes7 = 124124
+    val likes8 = 23
 
     println(getWord(likes1))
     println(getWord(likes2))
@@ -16,28 +18,15 @@ fun main(){
     println(getWord(likes4))
     println(getWord(likes5))
     println(getWord(likes6))
+    println(getWord(likes7))
+    println(getWord(likes8))
 }
 
 fun getWord(likes:Int):String{
-    val last = likes-((likes/10)*10)
-    var result = likes.toString();
-    if (likes <= 14) {
-        if (likes == 1)
-            result = "$result $likesWord1"
-        if (likes in 2..4)
-            result = "$result $likesWord3"
-        if(likes in 5..14 || likes == 0)
-            result = "$result $likesWord2"
-    } else {
-        if(last == 1) {
-            result = "$result $likesWord1"
-        }
-        if(last in 2..4) {
-            result = "$result $likesWord3"
-        }
-        if((last in 5..9) || last == 0) {
-            result = "$result $likesWord2"
-        }
-    }
+    var result = likes.toString()
+    if(likes % 100 in 5..20 || likes == 0) result += " $likesWord2"
+    else if(likes % 10 == 1) result += " $likesWord1"
+    else if(likes % 10 in 2..4) result += " $likesWord3"
+    else result = likesWord3;
     return result
 }
